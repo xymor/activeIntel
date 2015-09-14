475f6331f31324de9744e023252a3c39e1aa9295
ideais <raphaelmiranda@gmail.com>
2015-09-14 20:40:52 -0300

    +Visit

--- analytics/server.go
+++ analytics/server.go
@@ -5,10 +5,21 @@ import (
   "net/http"
   "time"
   "github.com/twinj/uuid"
+  "log"
+  "os"
+  "strings"
+  "gopkg.in/mgo.v2"
+  "gopkg.in/mgo.v2/bson"
 )
 
+var mongoURI = os.Getenv("MONGOURI")
+
+var session *mgo.Session
+
 func main() {
+  log.Println("Starting")
   r := gin.Default()
+  setupMongo()
 
   r.GET("/track", func(c *gin.Context) {
   	
@@ -20,11 +31,59 @@ func main() {
 			Value: uuid.NewV4().String(),
 			Path:  "/",
 		} 
-  		http.SetCookie(c.Writer, cookie)
+  	http.SetCookie(c.Writer, cookie)
 	}
 	out := cookie.Value		
-
+    saveVisit(NewVisit(cookie.Value, c.Request))
     c.JSON(200, gin.H{"sucess": out})
   })
   r.Run(":8000")
+}
+
+type Visit struct {
+        ID bson.ObjectId `bson:"_id,omitempty"`
+        Timestamp time.Time 
+        IP string
+        Agent string
+        Cookie string
+        Referer string
+}
+
+func NewVisit(cookieValue string, r *http.Request) *Visit {
+    v := new(Visit)
+    v.Timestamp = time.Now()
+    v.Agent = r.UserAgent()
+    v.Referer = r.Referer()
+    v.IP = strings.Split(r.RemoteAddr, ":")[0] //remember X-Forwarded-For
+    v.Cookie = cookieValue
+    return v
+}
+
+func saveVisit(visit *Visit) {
+  log.Println(visit)
+}
+
+func setupMongo() {
+        session, err := mgo.Dial(mongoURI)
+        if err != nil {
+                panic(err)
+        }
+        defer session.Close()
+
+        session.SetMode(mgo.Monotonic, true)
+
+        // c := session.DB("test").C("people")
+        // err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+        //          &Person{"Cla", "+55 53 8402 8510"})
+        // if err != nil {
+        //         log.Fatal(err)
+        // }
+
+        // result := Person{}
+        // err = c.Find(bson.M{"name": "Ale"}).One(&result)
+        // if err != nil {
+        //         log.Fatal(err)
+        // }
+
+        // fmt.Println("Phone:", result.Phone)
 }
